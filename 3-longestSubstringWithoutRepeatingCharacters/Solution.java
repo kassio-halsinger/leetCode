ce78d57b7f285ba44a7db1f1878126313fb30255
kassio-halsinger <halsink@wwu.edu>
2023-04-05 15:06:21 -0700

    Add string==null case solution

--- 3-longestSubstringWithoutRepeatingCharacters/Solution.java
+++ 3-longestSubstringWithoutRepeatingCharacters/Solution.java
@@ -1,2 +1,20 @@
 //Author: Kassio Halsinger
+//Last Updated: 4/5/23
 
+class Solution {
+    /* Given a string s, return the length of the longest substring...
+     * ...w/o repeating chars.
+     * 
+     */ 
+    public int lengthOfLongestSubstring(String s) {
+        //"abcabcbb" -> "abc" -> 3
+        //"a" -> "a" -> 1
+        //"bbbbbb" -<
+
+        //"<null>" -> 0
+        if (s == null){
+            return 0;
+        }
+
+    }
+}
