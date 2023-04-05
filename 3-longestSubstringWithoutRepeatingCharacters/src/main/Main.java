460581a237cae503bc9d3037561787417c84b953
kassio-halsinger <halsink@wwu.edu>
2023-04-05 16:18:45 -0700

    Fix test case 3 with counter. Fail others

--- 3-longestSubstringWithoutRepeatingCharacters/src/main/Main.java
+++ 3-longestSubstringWithoutRepeatingCharacters/src/main/Main.java
@@ -19,5 +19,18 @@ class Main{
         String s3 = "abccccdefg";
         int ans3 = solution.lengthOfLongestSubstring(s3);
         System.out.println("s3 result: "+ans3);
+
+        String s4 = "abcabcbb";
+        Solution solution = new Solution();
+        int ans4 = solution.lengthOfLongestSubstring(s1);
+        System.out.println("s1 result: "+ans1);
+
+        String s5 = "bbbbbb";
+        int ans5 = solution.lengthOfLongestSubstring(s2);
+        System.out.println("s2 result: "+ans2);
+
+        String s6 = "pwwkew";
+        int ans6 = solution.lengthOfLongestSubstring(s3);
+        System.out.println("s3 result: "+ans3);
     }
 }
--- 3-longestSubstringWithoutRepeatingCharacters/src/main/Solution.java
+++ 3-longestSubstringWithoutRepeatingCharacters/src/main/Solution.java
@@ -4,6 +4,7 @@ package src.main;
 //Last Updated: 4/5/23
 
 import java.util.*;
+import java.math.*;
 
 class Solution {
     /* Given a string s, return the length of the...
@@ -23,17 +24,23 @@ class Solution {
         //which has aleady appeared as we traverse
         //through the String s
         Set<String> set = new HashSet<>();
+        int solution = 0;
+        int count = 0;
 
         //until at the end of the string
-        for (int i = 0; i < s.length(); i++){
+        for (int i = 0; i < s.length(); i++){         
+            count++;
+            
             //check if second letter
             //was previously added to set;
             //if not, add it
             if (!set.add(s.substring(i,i+1))){
                 //if so, we have found a repeat char
-                return set.size();
+                solution = Math.max(solution,set.size());
+                set = new HashSet<>();
+                count = 1;
             }
         }
-        return set.size();
+        return Math.max(set.size(), count);
     }
 }
